6163b2e5030b230020db9e98b6b57d028fd95bcd
ldzbxtn <86514762+ldzbxtn@users.noreply.github.com>
2022-10-22 17:35:08 +0900

    [Silver IV] Title: 보물, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+result = 0
+
+for i in range(N):
+    result += A[i]*max(B)
+    B.remove(max(B))
+    
+print(result)
